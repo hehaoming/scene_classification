8c59d5b2819bfc97bd327c5aef11545bfd1c92dc
hehaoming <hehaoming110@qq.com>
2019-11-07 10:21:14 +0800

    增加了模型模块

---
+++ main/deep_learning/algorithm/model_senet.py
@@ -0,0 +1,12 @@
+import tensorflow as ts
+from tensorflow import keras
+import numpy as np
+from main.deep_learning.data_preprocessing.read_data import DataSet
+
+
+class SeNet:
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
